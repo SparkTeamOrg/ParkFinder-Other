ca309c59d7c63ecac3adaef55b7f4fc211d05fc0
Radovan Draskovic <73-2021@pmf.kg.ac.rs>
2024-12-27 16:39:28 +0100

    Add protected asset serving and dynamic screenshot loading in HTML

--- server.py
+++ server.py
@@ -20,13 +20,37 @@ def serve_styles(filename):
 def serve_assets(filename):
     return send_from_directory('src/assets', filename)
 
+@app.route('/protected_assets/<path:filename>')
+def serve_protected_assets(filename):
+    hash = request.args.get('hash')
+    if hash == VALID_HASH:
+        return send_from_directory('src/protected_assets', filename)
+    else:
+        return "You do not have access to this resource.", 403
+    
+@app.route('/src/protected_assets/<path:filename>')
+def serve_protected_assets_from_src(filename):
+    hash = request.args.get('hash')
+    if hash == VALID_HASH:
+        return send_from_directory('src/protected_assets', filename)
+    else:
+        return "You do not have access to this resource.", 403
+    
+@app.route('/src/apks/<path:filename>')
+def serve_apks_from_src(filename):
+    hash = request.args.get('hash')
+    if hash == VALID_HASH:
+        return send_from_directory('src/apks', filename)
+    else:
+        return "You do not have access to this resource.", 403
+
 @app.route('/download/<filename>')
 def download_file(filename):
     hash = request.args.get('hash')
     if hash == VALID_HASH:
         return send_from_directory('src/apks', filename, as_attachment=True)
     else:
-        return "Invalid secret", 403
+        return "You do not have access to this resource.", 403
 
 if __name__ == '__main__':
     from waitress import serve
